422713a3d654698304027981b3f8a11e7c57d229
supun64 <l.w.supunmadusanka64@gmail.com>
2022-05-18 10:57:18 +0530

    implemented unit testing

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,84 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.persistentExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest{
+
+    private static ExpenseManager expenseManager;
+
+
+    @Before
+    public void setup() throws ExpenseManagerException {
+        Context context = ApplicationProvider.getApplicationContext();
+        expenseManager = new persistentExpenseManager(context);
     }
+
+    @Test
+    public void testAddTransaction(){
+        // This get holds the previous transaction log count
+        int previousTransactionCount = expenseManager.getTransactionLogs().size();
+
+        expenseManager.getTransactionsDAO().logTransaction(
+                new Date(),
+                "190375K",
+                ExpenseType.EXPENSE,
+                12000);
+
+        int newTransactionCount = expenseManager.getTransactionLogs().size();
+
+        // This will ensure that a transaction is added
+        Assert.assertEquals(previousTransactionCount + 1, newTransactionCount);
+
+
+    }
+
+    @Test
+    public void testAddAccount(){
+
+        List<String> accountNumbers = expenseManager.getAccountNumbersList();
+
+        Random rand = new Random();
+        String randAccountNo = String.valueOf(rand.nextInt(10000));
+
+        // Lets confirm that the account number randAccountNo is not in the database
+        Assert.assertFalse(accountNumbers.contains(randAccountNo));
+
+        // adding the randAccountNo to the database
+        expenseManager.addAccount(
+                randAccountNo,
+                "Sampath Bank",
+                "Amal",
+                99999
+        );
+
+        // checking account is successfully added
+
+        accountNumbers = expenseManager.getAccountNumbersList();
+
+        Assert.assertTrue(accountNumbers.contains(randAccountNo));
+
+
+    }
+
+
 }
